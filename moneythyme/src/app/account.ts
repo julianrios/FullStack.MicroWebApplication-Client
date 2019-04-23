2f58e0d5e5709e2f0f164286d94ea0af79cd5da3
Michael Krohn <michaelkrohn@zipcoders-MacBook-Pro-96.local>
2019-04-23 18:38:16 -0400

    restructured project

--- moneythyme/src/app/account.ts
+++
@@ -1,5 +0,0 @@
-export class Account {
-  id: number;
-  profileID: number;
-  balance: number;
-}
--- moneythyme/src/app/profile.ts
+++
@@ -1,7 +0,0 @@
-export class Profile {
-  createProfile(name: string): any {
-    throw new Error('Method not implemented.');
-  }
-  id: number;
-  name: string;
-}
